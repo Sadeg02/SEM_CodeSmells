f1135db9dda3890a611382a5ab8a67192681124e
Sadeg02 <mich2002m@gmail.com>
2025-12-18 08:21:47 +0100

    fix ProductQuantity: delete redundant assignments

--- src/model/ProductQuantity.ts
+++ src/model/ProductQuantity.ts
@@ -1,10 +1,8 @@
 import {Product} from "./Product"
 
 export class ProductQuantity {
-
+    // deleted redundant assignments (public readonly already assigns them)
     constructor(public readonly product: Product,
                 public readonly quantity: number) {
-        this.product = product;
-        this.quantity = quantity;
     }
 }
