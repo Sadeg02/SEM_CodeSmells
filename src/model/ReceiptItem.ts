8663cca2dc7845f697aadcd3aabccc00ae9c74ed
Sadeg02 <mich2002m@gmail.com>
2025-12-18 08:22:52 +0100

    fix ReceiptItem: add missing readonly to totalPrice

--- src/model/ReceiptItem.ts
+++ src/model/ReceiptItem.ts
@@ -1,10 +1,10 @@
 import {Product} from "./Product"
 
 export class ReceiptItem {
-
+    // added readonly to totalPrice (was missing, others had it)
     public constructor(public readonly product: Product,
                        public readonly quantity: number,
                        public readonly price: number,
-                       public totalPrice: number) {
+                       public readonly totalPrice: number) {
     }
 }
