004b3f6d9ff397d5bf9f36e1a8987166228d32bb
Sadeg02 <mich2002m@gmail.com>
2025-12-18 08:22:22 +0100

    fix Offer: delete redundant getter

--- src/model/Offer.ts
+++ src/model/Offer.ts
@@ -2,14 +2,9 @@ import {Product} from "./Product"
 import {SpecialOfferType} from "./SpecialOfferType"
 
 export class Offer {
-
+    // deleted getProduct() - product is already public readonly so getter is useless
     public constructor(public readonly offerType: SpecialOfferType,
                        public readonly product: Product,
                        public readonly argument: number) {
     }
-
-    getProduct(): Product {
-        return this.product;
-    }
-
 }
